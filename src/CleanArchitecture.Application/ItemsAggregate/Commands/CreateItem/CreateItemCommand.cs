20bdd4628b0701b3a1158dc81bc563bcf2d13e8f
Alexander Chetcuti <chetcuti.alex@gmail.com>
2022-04-13 19:38:19 +0200

    Created command and handler for command

---
+++ src/CleanArchitecture.Application/ItemsAggregate/Commands/CreateItem/CreateItemCommand.cs
@@ -0,0 +1,18 @@
+﻿using CleanArchitecture.Application.ItemsAggregate.Responses;
+using MediatR;
+
+namespace CleanArchitecture.Application.ItemsAggregate.Commands.CreateItem
+{
+    public class CreateItemCommand : IRequest<ItemResponse>
+    {
+        public ItemResponse? Response { get; set; }   
+    }
+
+    public class CreateItemCommandHandler : IRequestHandler<CreateItemCommand, ItemResponse>
+    {
+        public Task<ItemResponse> Handle(CreateItemCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/CleanArchitecture.Application/ItemsAggregate/Request/ItemRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArchitecture.Application.ItemsAggregate.Request
+{
+    public class ItemRequest
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+    }
+}
